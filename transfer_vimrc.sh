fe0cb40c99c175fe2beb891da3e7fd5c4406c263
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-10-15 08:33:34 -0700

    added system-unique transferring from local vimrc to ~/.vimrc

--- transfer_vimrc.sh
+++ transfer_vimrc.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+system_type="$(uname -a)"
 
-ln -is vimrc.vim ~/.vimrc
+if [[ $system_type =~ "MINGW64" ]]; then
+  cp vimrc.vim ~/.vimrc && echo "vimrc copied successfully to ~/.vimrc on your MINGW64 system"
+elif [[ $system_type =~ "Darwin" ]]; then
+  ln -si vimrc.vim ~/.vimrc && echo "vimrc copied successfully to ~/.vimrc on your Darwin system"
+else
+  echo "System type \"$system_type\" not recognized, no vimrc file transfer was automated"
+fi
