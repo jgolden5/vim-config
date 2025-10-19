78123744bc2d58fb5602ad191838c161b9cc75e4
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-10-19 09:38:55 -0700

    improved transfer_vimrc.sh script

--- transfer_vimrc.sh
+++ transfer_vimrc.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 system_type="$(uname -a)"
-
-if [[ $system_type =~ "MINGW64" ]]; then
-  cp vimrc.vim ~/.vimrc && echo "vimrc copied successfully to ~/.vimrc on your MINGW64 system"
-elif [[ $system_type =~ "Darwin" ]]; then
-  ln -si vimrc.vim ~/.vimrc && echo "vimrc copied successfully to ~/.vimrc on your Darwin system"
+cp vimrc.vim ~/.vimrc 
+if [[ $? == 0 ]]; then
+  if [[ $system_type =~ "MINGW64" ]]; then
+    echo "vimrc copied successfully to ~/.vimrc on your MINGW64 system"
+  elif [[ $system_type =~ "Darwin" ]]; then
+    echo "vimrc copied successfully to ~/.vimrc on your Darwin system"
+  else
+    echo "System type \"$system_type\" not recognized, no vimrc file transfer was automated"
+  fi
 else
-  echo "System type \"$system_type\" not recognized, no vimrc file transfer was automated"
+  echo "something went wrong copying vimrc.vim to ~/.vimrc"
 fi
