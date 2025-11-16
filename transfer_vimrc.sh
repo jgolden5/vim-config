099a0f36a7bf4f449766ca0cd8bd2b19725dc995
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-11-16 09:45:50 -0700

    added AI search hotkeys to vimrc

--- transfer_vimrc.sh
+++ transfer_vimrc.sh
@@ -3,9 +3,10 @@ system_type="$(uname -a)"
 message=
 if [[ $? == 0 ]]; then
   if [[ $system_type =~ "MINGW64" ]]; then
+    sed -i 's/!open/!start/g' vimrc.vim
     message="vimrc copied successfully to ~/.vimrc on your MINGW64 system"
-    sed -i 's/open/start/g' vimrc.vim
   elif [[ $system_type =~ "Darwin" ]]; then
+    sed -i '' 's/!start/!open/g' vimrc.vim
     message="vimrc copied successfully to ~/.vimrc on your Darwin system"
   else
     message="System type \"$system_type\" not recognized, no vimrc file transfer was automated"
