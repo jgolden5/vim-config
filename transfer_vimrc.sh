caea98cf2f82fc2fe64818d7d25ee44167a03151
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-11-26 09:57:04 -0700

    fixed vim keymaps for links in msys2

--- transfer_vimrc.sh
+++ transfer_vimrc.sh
@@ -3,7 +3,8 @@ system_type="$(uname -a)"
 message=
 if [[ $? == 0 ]]; then
   if [[ $system_type =~ "MINGW64" ]]; then
-    sed -i 's/!open/!start/g' vimrc.vim
+    sed -i 's/!open/!explorer/g' vimrc.vim #dang it, internet explorer!
+    sed -i 's/pbpaste/powershell -command Get-Clipboard/g' vimrc.vim
     message="vimrc copied successfully to ~/.vimrc on your MINGW64 system"
   elif [[ $system_type =~ "Darwin" ]]; then
     sed -i '' 's/!start/!open/g' vimrc.vim
